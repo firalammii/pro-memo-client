c94c19cce15122ddf632e3432a3ab9e2768f9988
firalammii <90164296+firalammii@users.noreply.github.com>
2023-03-17 20:43:44 +0300

    some small changes

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import moment from 'moment';
+
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
 import { useDispatch } from 'react-redux';
 
 import { deletePost, likePost } from '../../actions/postActions';
@@ -11,25 +16,36 @@ const Post = ({ post, updateSelected }) => {
 
     return (
         <div className='post'>
-            <img className='pic' src={pic} alt='pic from db' />
+            <div className='image'>
+                <img className='pic' src={pic} alt='pic from db' />
+                <div className='absolute-left'>
+                    <p className='creator'>{creator}</p>
+                    <p className='date'>{moment(date).fromNow()} </p>
+                </div>
+                <MenuIcon className='absolute-right' onClick={() => updateSelected(post)} />
+            </div>
             <div className='post-contents'>
-                <h4 className='title'>{title}</h4>
+                <h3 className='title'>{title}</h3> <br />
                 <p className='body'>{body}</p>
-                <p className='creator'>post by: <span id='creator'>{creator}</span></p>
+                <br />
                 <p className='tags'>{tags.map((tag, i) => <span key={i}>{`#${tag}`}</span>)}</p>
                 <div className='date-n-likes-con'>
                     <div className='likes' onClick={() => dispatch(likePost(post))}>
                         <button className='like-btn' >👍</button>{likes < 100 ? likes : `${(likes / 1000).toFixed(2)}k`}
                     </div>
-                    <p className='date'>{moment(date).fromNow()} </p>
                 </div>
                 <div className='update-n-delete-btns-con'>
                     <button className='update-n-delete-btns ' onClick={() => updateSelected(post)}>UPDATE</button>
-                    <button className='update-n-delete-btns ' onClick={() => dispatch(deletePost(post._id))}>DELETE</button>
+                    <button className='update-n-delete-btns ' onClick={() => dispatch(deletePost(post._id))}><DeleteForeverRoundedIcon /></button>
                 </div>
             </div>
+
         </div>
+
     );
-};
+}
+
+
+
 
 export default Post;
